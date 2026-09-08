09e7d3ec1807b2aa11ab5d5d05665f19d7c34635
vhicthorace2 <183820191+vhicthorace2@users.noreply.github.com>
2026-09-08 18:36:13 +0100

    configure the vercel.json file for deployment for the fifth time

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import  ReactDOM  from 'react-dom/client'
-import App from './App.jsx'
-import { ThemeProvider } from './component/theme.jsx';
+import App from './App.jsx';
+import { ThemeProvider } from './component/theme.jsx'; // Verify 'component' matches your folder name
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
